29fe1ff8e8da22672831ce89c5a08ff58594524b
Victor Borja <vborja@apache.org>
2025-05-16 11:51:21 -0600

    More functional

--- fx.go
+++ fx.go
@@ -1,8 +1,10 @@
 package fx
 
-type immediate[V any] func() V
-type suspended[S, V any] func(S) Fx[S, V]
-type Fx[S, V any] func() (immediate[V], suspended[S, V])
+type Fx[S, V any] struct {
+	imm func() V
+	sus func(S) Fx[S, V]
+	hlt func()
+}
 
 type FxPure[V any] = Fx[Nil, V]
 type FxNil = FxPure[Nil]
@@ -17,164 +19,135 @@ func Pure[V any](v V) FxPure[V] {
 	return Const[Nil](v)
 }
 
+func identity[V any](v V) V { return v }
+
+func Const[S, V any](v V) Fx[S, V] {
+	return Fx[S, V]{imm: func() V { return v }}
+}
+
+func Pending[S, V any](f func(S) Fx[S, V]) Fx[S, V] {
+	return Fx[S, V]{sus: f}
+}
+
 func Func[S, V any](f func(S) V) Fx[S, V] {
-	return func() (immediate[V], suspended[S, V]) {
-		return nil, func(s S) Fx[S, V] {
-			return func() (immediate[V], suspended[S, V]) {
-				v := func() V { return f(s) }
-				return v, nil
-			}
-		}
-	}
+	return Pending(func(s S) Fx[S, V] { return Const[S](f(s)) })
+}
+
+func Apply[F ~func(I) O, I, O any](i I) Fx[F, O] {
+	return Map(Ctx[F](), func(f F) O { return f(i) })
 }
 
 func Ctx[V any]() Fx[V, V] {
-	return Func(func(v V) V { return v })
+	return Func(identity[V])
 }
 
 // An effect that will never be continued.
 func Halt[S, V any]() Fx[S, V] {
-	return nil
+	return Fx[S, V]{hlt: func() {}}
 }
 
 // Replace with y if x is already Halted. Otherwise x continues.
 func Replace[S, V any](y func() Fx[S, V]) func(Fx[S, V]) Fx[S, V] {
 	return func(x Fx[S, V]) Fx[S, V] {
-		if x == nil {
+		if x.hlt != nil {
 			return y()
 		}
-		imm, sus := x()
-		if imm != nil {
-			return Const[S](imm())
+		if x.imm != nil {
+			return Const[S](x.imm())
 		}
-		return func() (immediate[V], suspended[S, V]) {
-			return nil, func(s S) Fx[S, V] {
-				return Replace(y)(sus(s))
-			}
-		}
+		return Pending(func(s S) Fx[S, V] { return Replace(y)(x.sus(s)) })
 	}
 }
 
-func cont[T, U, S, V any](r func(T) S, f func(immediate[V]) Fx[T, U]) func(Fx[S, V]) Fx[T, U] {
+// Continue an effect by transforming its immediate value into another effect.
+func Cont[T, U, S, V any](cmap func(T) S, fmap func(V) Fx[T, U]) func(Fx[S, V]) Fx[T, U] {
 	return func(e Fx[S, V]) Fx[T, U] {
-		if e == nil {
-			return nil
+		if e.hlt != nil {
+			return Halt[T, U]()
 		}
-		imm, sus := e()
-		if imm != nil {
-			return f(imm)
+		if e.imm != nil {
+			return fmap(e.imm())
 		}
-		return func() (immediate[U], suspended[T, U]) {
-			return nil, func(t T) Fx[T, U] {
-				return cont(r, f)(sus(r(t)))
-			}
-		}
+		return Pending(func(t T) Fx[T, U] { return Cont(cmap, fmap)(e.sus(cmap(t))) })
 	}
 }
 
 type And[A, B any] func() (A, B)
 
+func rswap[A, B any](ba And[B, A]) And[A, B] {
+	var ab And[A, B] = func() (A, B) {
+		b, a := ba()
+		return a, b
+	}
+	return ab
+}
+
+func left[A, B any](ab And[A, B]) A {
+	a, _ := ab()
+	return a
+}
+
+func right[A, B any](ab And[A, B]) B {
+	_, b := ab()
+	return b
+}
+
 func ContraMap[V, S, R any](f func(R) S) func(Fx[S, V]) Fx[R, V] {
 	return func(e Fx[S, V]) Fx[R, V] {
-		if e == nil {
-			return nil
+		if e.hlt != nil {
+			return Halt[R, V]()
 		}
-		imm, sus := e()
-		if imm != nil {
-			return Const[R](imm())
+		if e.imm != nil {
+			return Const[R](e.imm())
 		}
-		return func() (immediate[V], suspended[R, V]) {
-			return nil, func(r R) Fx[R, V] {
-				s := f(r)
-				return ContraMap[V](f)(sus(s))
-			}
-		}
+		return Pending(func(r R) Fx[R, V] {
+			return ContraMap[V](f)(e.sus(f(r)))
+		})
 	}
 }
 
 func MapM[S, U, V any](f func(U) Fx[S, V]) func(Fx[S, U]) Fx[S, V] {
-	id := func(s S) S { return s }
-	return cont(id, func(u immediate[U]) Fx[S, V] {
-		v := f(u())
-		return cont(id, func(v immediate[V]) Fx[S, V] {
-			return Const[S](v())
-		})(v)
-	})
+	return FlatCont(identity, identity, f)
 }
 
-func MapT[S, V, U any](f func(V) U) func(Fx[S, V]) Fx[S, U] {
-	return MapM(func(v V) Fx[S, U] {
-		u := f(v)
-		return Const[S](u)
-	})
+func MapH[S, V, U any](f func(V) U) func(Fx[S, V]) Fx[S, U] {
+	return MapM(func(v V) Fx[S, U] { return Const[S](f(v)) })
 }
 
 func Map[S, V, U any](e Fx[S, V], f func(V) U) Fx[S, U] {
-	return MapT[S](f)(e)
+	return MapH[S](f)(e)
 }
 
 func FlatMap[A, U, B, V any](e Fx[A, U], f func(U) Fx[B, V]) Fx[And[A, B], V] {
-	return FlatMapT[A](f)(e)
+	return FlatMapH[A](f)(e)
 }
 
-func FlatMapT[A, U, B, V any](f func(U) Fx[B, V]) func(Fx[A, U]) Fx[And[A, B], V] {
-	a := func(ab And[A, B]) A {
-		a, _ := ab()
-		return a
-	}
-	b := func(ab And[A, B]) B {
-		_, b := ab()
-		return b
-	}
-	return cont(a, func(u immediate[U]) Fx[And[A, B], V] {
-		return cont(b, func(v immediate[V]) Fx[And[A, B], V] {
-			return Const[And[A, B]](v())
-		})(f(u()))
-	})
+func FlatMapH[A, U, B, V any](f func(U) Fx[B, V]) func(Fx[A, U]) Fx[And[A, B], V] {
+	return FlatCont[And[A, B]](left, right, f)
+}
+
+func FlatCont[N, A, U, B, V any](amap func(N) A, bmap func(N) B, fmap func(U) Fx[B, V]) func(Fx[A, U]) Fx[N, V] {
+	return Cont(amap, func(u U) Fx[N, V] { return Cont(bmap, Const[N, V])(fmap(u)) })
 }
 
 func AndNil[S, V any](e Fx[S, V]) Fx[And[S, Nil], V] {
-	fst := func(n And[S, Nil]) S {
-		s, _ := n()
-		return s
-	}
-	return cont(fst, func(v immediate[V]) Fx[And[S, Nil], V] {
-		return Const[And[S, Nil]](v())
-	})(e)
+	return Cont[And[S, Nil], V](left, Const)(e)
 }
 
 func AndSwap[A, B, V any](e Fx[And[A, B], V]) Fx[And[B, A], V] {
-	swp := func(ba And[B, A]) And[A, B] {
-		var ab And[A, B] = func() (A, B) {
-			b, a := ba()
-			return a, b
-		}
-		return ab
-	}
-	return cont(swp, func(v immediate[V]) Fx[And[B, A], V] {
-		return Const[And[B, A]](v())
-	})(e)
+	return Cont[And[B, A], V](rswap, Const)(e)
 }
 
 func AndJoin[A, B, V any](e Fx[A, Fx[B, V]]) Fx[And[A, B], V] {
-	return FlatMap(e, func(f Fx[B, V]) Fx[B, V] { return f })
+	return FlatMap(e, identity)
 }
 
 func AndDisjoin[A, B, V any](e Fx[And[A, B], V]) Fx[A, Fx[B, V]] {
-	return func() (immediate[Fx[B, V]], suspended[A, Fx[B, V]]) {
-		return nil, func(a A) Fx[A, Fx[B, V]] {
-			x := ProvideLeft(e, a)
-			return Const[A](x)
-		}
-	}
+	return Pending(func(a A) Fx[A, Fx[B, V]] { return Const[A](ProvideLeft(e, a)) })
 }
 
 func AndCollapse[A, V any](e Fx[And[A, A], V]) Fx[A, V] {
-	return func() (immediate[V], suspended[A, V]) {
-		return nil, func(a A) Fx[A, V] {
-			return ProvideLeft(e, a)
-		}
-	}
+	return Pending(func(a A) Fx[A, V] { return ProvideLeft(e, a) })
 }
 
 func ProvideFirstLeft[A, B, C, V any](e Fx[And[And[A, C], B], V], a A) Fx[And[C, B], V] {
@@ -202,38 +175,24 @@ func ProvideRight[A, B, V any](e Fx[And[A, B], V], b B) Fx[A, V] {
 }
 
 func ProvideLeft[A, B, V any](e Fx[And[A, B], V], a A) Fx[B, V] {
-	if e == nil {
-		return nil
+	if e.hlt != nil {
+		return Halt[B, V]()
 	}
-	imm, sus := e()
-	if imm != nil {
-		return Const[B](imm())
+	if e.imm != nil {
+		return Const[B](e.imm())
 	}
-	return func() (immediate[V], suspended[B, V]) {
-		return nil, func(b B) Fx[B, V] {
-			var ab And[A, B] = func() (A, B) { return a, b }
-			for {
-				e = sus(ab)
-				if e == nil {
-					return nil
-				}
-				imm, sus = e()
-				if imm != nil {
-					return Const[B](imm())
-				}
+	return Pending(func(b B) Fx[B, V] {
+		var ab And[A, B] = func() (A, B) { return a, b }
+		for {
+			e = e.sus(ab)
+			if e.hlt != nil {
+				return Halt[B, V]()
+			}
+			if e.imm != nil {
+				return Const[B](e.imm())
 			}
 		}
-	}
-}
-
-func Apply[F ~func(I) O, I, O any](i I) Fx[F, O] {
-	return Map(Ctx[F](), func(f F) O { return f(i) })
-}
-
-func Const[S, V any](v V) Fx[S, V] {
-	return func() (immediate[V], suspended[S, V]) {
-		return func() V { return v }, nil
-	}
+	})
 }
 
 func Suspend[A ~func(I) Fx[B, O], B, I, O any](i I) Fx[And[A, B], O] {
@@ -250,14 +209,12 @@ func Handle[F ~func(Fx[And[A, B], O]) Fx[B, O], A ~func(I) Fx[B, O], B, I, O any
 
 func Eval[V any](e Fx[Nil, V]) V {
 	for {
-		if e == nil {
+		if e.hlt != nil {
 			panic("tried to evaluate halted effect. try using fx.Replace with another effect.")
 		}
-		imm, sus := e()
-		if imm != nil {
-			v := imm()
-			return v
+		if e.imm != nil {
+			return e.imm()
 		}
-		e = sus(PNil)
+		e = e.sus(PNil)
 	}
 }
